856c085c0edf3d7146bdc22e6794231ec3e14f07
beepo <gooblywoobly@[127.0.0.1]>
2025-09-30 15:07:06 -0400

    thinkle

--- src/pipeline/machine.rs
+++ src/pipeline/machine.rs
@@ -31,12 +31,18 @@ pub enum MachineKind {
 
 #[derive(Component, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum MachineStatus {
-    Working(u64),
+    Working(Working),
     Full,
     LacksInput,
     Ready,
     CraftsFinished(u64),
     Idle,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Working {
+    ticks_remaining: u64,
+    amount: u64,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -72,10 +78,10 @@ pub struct MachineInputNew(Vec<Entity>);
 pub struct MachineOutputNew(Entity);
 
 #[derive(Component, Clone, Debug)]
-pub struct OutputBank(Vec<IoPort>);
+pub struct OutputBuffers(Vec<IoPort>);
 
 #[derive(Component, Clone, Debug)]
-pub struct MachineBuffer(Vec<IoPort>);
+pub struct InputBuffers(Vec<IoPort>);
 
 #[derive(Component, Clone, Copy, Debug)]
 pub struct Mult(u64);
@@ -94,12 +100,11 @@ pub struct Machine {
 pub fn tick_machines(mut machine_query: Query<(&Recipe, &mut MachineStatus, Option<&Mult>)>) {
     for(recipe, mut status, maybe_mult) in &mut machine_query {
         match *status {
-            MachineStatus::Working(ref mut ticks_remaining) => {
+            MachineStatus::Working(Working { ticks_remaining: ref mut ticks_remaining, amount}) => {
                 if let Some(mult) = maybe_mult {
                     if mult.0 > *ticks_remaining {
-                        let full_crafts = (mult.0 - *ticks_remaining) % recipe.ticks;
-                        *ticks_remaining = (mult.0 - *ticks_remaining) / recipe.ticks;
-                        *status = MachineStatus::CraftsFinished(full_crafts);
+                        *ticks_remaining = (mult.0 - *ticks_remaining) / recipe.ticks.max(1);
+                        *status = MachineStatus::CraftsFinished(amount);
                     } else {
                         *ticks_remaining -= mult.0;
                     }
@@ -111,234 +116,247 @@ pub fn tick_machines(mut machine_query: Query<(&Recipe, &mut MachineStatus, Opti
                     }
                 }
             },
-            MachineStatus::Ready => {
-                if let Some(mult) = maybe_mult {
-                    let full_crafts = mult.0 % recipe.ticks;
-                    *status = MachineStatus::CraftsFinished(full_crafts);
-                } else {
-                    *status = MachineStatus::Working(recipe.ticks - 1);
-                }
-            },
-            MachineStatus::LacksInput => {
-                *status = MachineStatus::Idle
-            },
             _ => {},
         }
     }
 }
 
-pub fn try_craft_new(mut machine_query: Query<(&mut MachineBuffer, &mut OutputBank, &mut MachineStatus, &Recipe)>) {
-    for (mut buffer, mut outputs, mut status, recipe) in machine_query {
+pub fn try_craft_new(mut machine_query: Query<(&MachineOutputNew, &mut OutputBuffers, &mut MachineStatus, &Recipe)>) {
+    for (dest, mut outputs, mut status, recipe) in machine_query {
         if let MachineStatus::CraftsFinished(num_crafts) = *status {
+            
         }
     }
 }
 
-pub fn ready_craft(mut machine_query: Query<(&mut MachineBuffer, &mut MachineStatus, &Recipe, Option<&Mult>)>) {
-    for (mut buffer, mut status, recipe, mult) in machine_query {
-        let possible_crafts = {
-            let mut possible_crafts = mult.unwrap_or(&Mult(1)).0;
+pub fn ready_craft(mut machine_query: Query<(&mut InputBuffers, &OutputBuffers, &mut MachineStatus, &Recipe, Option<&Mult>)>) {
+    for (mut inputs, outputs, mut status, recipe, mult) in machine_query {
+        if *status != MachineStatus::Idle {
+            continue;
+        }
+        let mut possible_crafts = mult.unwrap_or(&Mult(1)).0;
 
-            for output in recipe.outputs {
-                if let Some(output) = output {
-                    let buffered = buffer.0.iter().fold(0, |acc, port| {
-                        if port.item_type == output.item_type {
-                            acc + port.buffer.current
-                        } else {
-                            acc
-                        }
-                    });
-
-                    let bungus = buffered / output.amount;
-                    if bungus == 0 {
-                        possible_crafts = 0;
-                        break;
-                    }
-
-                    possible_crafts = possible_crafts.min(bungus);
+        for output in recipe.outputs.iter().filter_map(|o| *o) {
+            let buffered = inputs.0.iter().fold(0, |acc, port| {
+                if port.item_type == output.item_type {
+                    acc + port.buffer.current
+                } else {
+                    acc
                 }
+            });
+
+            let bungus = buffered / output.amount;
+            if bungus == 0 {
+                possible_crafts = 0;
+                break;
             }
 
+            let bufferable = outputs.0.iter().fold(0, |acc, port| {
+                if port.item_type == output.item_type {
+                    acc + port.buffer.remaining()
+                } else {
+                    acc
+                }
+            });
 
+            let bungus2 = bufferable / output.amount;
+            if bungus2 == 0 {
+                possible_crafts = 0;
+                break;
+            }
+
+            possible_crafts = possible_crafts.min(bungus).min(bungus2);
         }
+
+        for output in recipe.outputs.iter().filter_map(|o| *o) {
+            let mut taken = output.amount * possible_crafts;
+
+            for input in inputs.0.iter_mut().filter(|i| i.item_type == output.item_type) {
+                let takeable = taken.abs_diff(input.buffer.current);
+                input.buffer.current -= takeable;
+                taken -= takeable;
+            }
+        }
+
+        *status = MachineStatus::Working(Working { ticks_remaining: recipe.ticks, amount: possible_crafts });
     }
 }
 
-impl Machine {
-    pub fn tick(&mut self) -> CraftStatus {
-        match self.status {
-            MachineStatus::Working(ref mut ticks_remaining) => {
-                *ticks_remaining -= 1;
+// impl Machine {
+//     pub fn tick(&mut self) -> CraftStatus {
+//         match self.status {
+//             MachineStatus::Working(ref mut ticks_remaining) => {
+//                 *ticks_remaining -= 1;
 
-                if *ticks_remaining == 0 {
-                    self.try_craft();
-                    CraftStatus::Complete
-                } else {
-                    CraftStatus::Incomplete
-                }
-            },
-            MachineStatus::Ready => {
-                if self.recipe.ticks == 1 {
-                    self.try_craft();
-                    CraftStatus::Complete
-                } else if self.recipe.ticks == 0 {
-                    CraftStatus::Complete  
-                } else {
-                    self.status = MachineStatus::Working(self.recipe.ticks - 1);
-                    CraftStatus::Incomplete
-                }
-            },
-            MachineStatus::LacksInput => {
-                self.try_ready();
-                CraftStatus::Incomplete
-            },
-            _ => CraftStatus::Incomplete,
-        }
-    }
+//                 if *ticks_remaining == 0 {
+//                     self.try_craft();
+//                     CraftStatus::Complete
+//                 } else {
+//                     CraftStatus::Incomplete
+//                 }
+//             },
+//             MachineStatus::Ready => {
+//                 if self.recipe.ticks == 1 {
+//                     self.try_craft();
+//                     CraftStatus::Complete
+//                 } else if self.recipe.ticks == 0 {
+//                     CraftStatus::Complete  
+//                 } else {
+//                     self.status = MachineStatus::Working(self.recipe.ticks - 1);
+//                     CraftStatus::Incomplete
+//                 }
+//             },
+//             MachineStatus::LacksInput => {
+//                 self.try_ready();
+//                 CraftStatus::Incomplete
+//             },
+//             _ => CraftStatus::Incomplete,
+//         }
+//     }
 
-    pub fn try_craft(&mut self) {
-        if self.status != MachineStatus::Working(0) {
-            return;
-        }
+//     pub fn try_craft(&mut self) {
+//         if self.status != MachineStatus::Working(0) {
+//             return;
+//         }
 
-        let mut ready = [false, false, false, false];
-        for i in 0..4 {
-            if let Some(item_stack) = self.recipe.outputs[i] {
-                if let Some(port) = self.output_ports[i] {
-                    if item_stack.item_type != port.item_type {
-                        panic!("Invalid output");
-                    }
+//         let mut ready = [false, false, false, false];
+//         for i in 0..4 {
+//             if let Some(item_stack) = self.recipe.outputs[i] {
+//                 if let Some(port) = self.output_ports[i] {
+//                     if item_stack.item_type != port.item_type {
+//                         panic!("Invalid output");
+//                     }
 
-                    if port.buffer.remaining() >= item_stack.amount {
-                        ready[i] = true;
-                    }
-                } else {
-                    panic!("Missing input port");
-                }
-            } else {
-                ready[i] = true;
-            }
-        }
+//                     if port.buffer.remaining() >= item_stack.amount {
+//                         ready[i] = true;
+//                     }
+//                 } else {
+//                     panic!("Missing input port");
+//                 }
+//             } else {
+//                 ready[i] = true;
+//             }
+//         }
 
-        if ready.iter().all(|b| *b) {
-            for i in 0..4 {
-                if let Some(item_stack) = self.recipe.outputs[i] {
-                    if let Some(port) = &mut self.output_ports[i] {
-                        port.buffer.current += item_stack.amount;
-                        println!("Crafted {:?} x{}", item_stack.item_type, item_stack.amount);
-                    }
-                }
-            }
+//         if ready.iter().all(|b| *b) {
+//             for i in 0..4 {
+//                 if let Some(item_stack) = self.recipe.outputs[i] {
+//                     if let Some(port) = &mut self.output_ports[i] {
+//                         port.buffer.current += item_stack.amount;
+//                         println!("Crafted {:?} x{}", item_stack.item_type, item_stack.amount);
+//                     }
+//                 }
+//             }
 
-            self.try_ready();
-        }
-    }
+//             self.try_ready();
+//         }
+//     }
 
-    /// Attempts to ready a craft by taking items from input ports and setting machine status
-    pub fn try_ready(&mut self) {
-        let mut ready = [false, false, false, false];
-        for i in 0..4 {
-            if let Some(item_stack) = self.recipe.inputs[i] {
-                if let Some(port) = self.input_ports[i] {
-                    if item_stack.item_type != port.item_type {
-                        panic!("Invalid input");
-                    }
+//     /// Attempts to ready a craft by taking items from input ports and setting machine status
+//     pub fn try_ready(&mut self) {
+//         let mut ready = [false, false, false, false];
+//         for i in 0..4 {
+//             if let Some(item_stack) = self.recipe.inputs[i] {
+//                 if let Some(port) = self.input_ports[i] {
+//                     if item_stack.item_type != port.item_type {
+//                         panic!("Invalid input");
+//                     }
 
-                    if port.buffer.current >= item_stack.amount {
-                        ready[i] = true;
-                    }
-                } else {
-                    panic!("Missing input port");
-                }
-            } else {
-                ready[i] = true;
-            }
-        }
+//                     if port.buffer.current >= item_stack.amount {
+//                         ready[i] = true;
+//                     }
+//                 } else {
+//                     panic!("Missing input port");
+//                 }
+//             } else {
+//                 ready[i] = true;
+//             }
+//         }
 
-        if ready.iter().all(|b| *b) {
-            for i in 0..4 {
-                if let Some(item_stack) = self.recipe.outputs[i] {
-                    if let Some(port) = &mut self.input_ports[i] {
-                        port.buffer.current -= item_stack.amount;
-                    }
-                }
-            }
+//         if ready.iter().all(|b| *b) {
+//             for i in 0..4 {
+//                 if let Some(item_stack) = self.recipe.outputs[i] {
+//                     if let Some(port) = &mut self.input_ports[i] {
+//                         port.buffer.current -= item_stack.amount;
+//                     }
+//                 }
+//             }
 
-            self.status = MachineStatus::Ready;
-        } else {
-            self.status = MachineStatus::LacksInput;
-        }
-    }
+//             self.status = MachineStatus::Ready;
+//         } else {
+//             self.status = MachineStatus::LacksInput;
+//         }
+//     }
 
-    pub fn is_starter(&self) -> bool {
-        match self.kind {
-            MachineKind::Producer => true,
-            _ => false,
-        }
-    }
+//     pub fn is_starter(&self) -> bool {
+//         match self.kind {
+//             MachineKind::Producer => true,
+//             _ => false,
+//         }
+//     }
 
-    pub fn outputs(&self) -> Vec<ItemStack> {
-        self.recipe.outputs.iter().filter_map(|inner| *inner).collect()
-    }
+//     pub fn outputs(&self) -> Vec<ItemStack> {
+//         self.recipe.outputs.iter().filter_map(|inner| *inner).collect()
+//     }
 
-    pub fn inputs(&self) -> Vec<ItemStack> {
-        self.recipe.inputs.iter().filter_map(|inner| *inner).collect()
-    }
+//     pub fn inputs(&self) -> Vec<ItemStack> {
+//         self.recipe.inputs.iter().filter_map(|inner| *inner).collect()
+//     }
 
-    pub fn get_output(&mut self, item_type: ItemType) -> Result<MachineOutput, MachineBindError> {
-        let mut idx: Option<usize> = None;
-        for i in 0..4 {
-            if let Some(port) = &self.output_ports[i] {
-                if port.is_free() && port.item_type == item_type {
-                    idx = Some(i);
-                    break;
-                }
-            }
-        }
+//     pub fn get_output(&mut self, item_type: ItemType) -> Result<MachineOutput, MachineBindError> {
+//         let mut idx: Option<usize> = None;
+//         for i in 0..4 {
+//             if let Some(port) = &self.output_ports[i] {
+//                 if port.is_free() && port.item_type == item_type {
+//                     idx = Some(i);
+//                     break;
+//                 }
+//             }
+//         }
 
-        if let Some(idx) = idx {
-            Ok(MachineOutput {
-                id: &mut self.output_id,
-                port: &mut self.output_ports[idx],
-                item_type: item_type,
-            })
-        } else {
-            Err(MachineBindError::NoFreeOutputs)
-        }
-    }
+//         if let Some(idx) = idx {
+//             Ok(MachineOutput {
+//                 id: &mut self.output_id,
+//                 port: &mut self.output_ports[idx],
+//                 item_type: item_type,
+//             })
+//         } else {
+//             Err(MachineBindError::NoFreeOutputs)
+//         }
+//     }
 
-    pub fn get_input(&mut self, item_type: ItemType) -> Result<MachineInput, MachineBindError> {
-        let mut idx: Option<usize> = None;
-        for i in 0..4 {
-            if let Some(port) = &self.input_ports[i] {
-                if port.is_free() && port.item_type == item_type {
-                    idx = Some(i);
-                    break;
-                }
-            }
-        }
+//     pub fn get_input(&mut self, item_type: ItemType) -> Result<MachineInput, MachineBindError> {
+//         let mut idx: Option<usize> = None;
+//         for i in 0..4 {
+//             if let Some(port) = &self.input_ports[i] {
+//                 if port.is_free() && port.item_type == item_type {
+//                     idx = Some(i);
+//                     break;
+//                 }
+//             }
+//         }
 
-        if let Some(idx) = idx {
-            Ok(MachineInput {
-                port: &mut self.input_ports[idx],
-            })
-        } else {
-            Err(MachineBindError::NoFreeOutputs)
-        }
-    }
+//         if let Some(idx) = idx {
+//             Ok(MachineInput {
+//                 port: &mut self.input_ports[idx],
+//             })
+//         } else {
+//             Err(MachineBindError::NoFreeOutputs)
+//         }
+//     }
 
-    pub fn new_storage(item_type: ItemType) -> Self {
-        Self {
-            kind: MachineKind::Storage,
-            output_id: None,
-            recipe: Recipe::storage_recipe(),
-            status: MachineStatus::LacksInput,
-            input_ports: [Some(item_type.into()), None, None, None],
-            output_ports: [None; 4],
-            mult: 1,
-        }
-    }
-}
+//     pub fn new_storage(item_type: ItemType) -> Self {
+//         Self {
+//             kind: MachineKind::Storage,
+//             output_id: None,
+//             recipe: Recipe::storage_recipe(),
+//             status: MachineStatus::LacksInput,
+//             input_ports: [Some(item_type.into()), None, None, None],
+//             output_ports: [None; 4],
+//             mult: 1,
+//         }
+//     }
+// }
 
 impl From<Recipe> for Machine {
     fn from(value: Recipe) -> Self {
